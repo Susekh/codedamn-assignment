c268a4660b75cc69d220216b084b8a21bf3282ac
Subh <subhranshukhilar@outlook.com>
2024-01-15 00:57:51 +0530

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import './styles/App.css'
-import GamePart from './components/gamePart';
 
 function App(){
   return (
     <>
       <div className="header-text"><h1>Pokemon Memory Game</h1></div>
-        
-      <div className="main-content-part">
-        <GamePart />
-      </div>
     </>
   )
 }
